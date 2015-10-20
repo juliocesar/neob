c4468ba10100d02b354f3c32b46ac849c235ef49
Julio Cesar Ody <julio@awesomebydesign.com>
2015-10-20 15:06:55 +1100

    Header and more lint fixes

--- test/components/app.spec.js
+++ test/components/app.spec.js
@@ -4,7 +4,6 @@
 import React from 'react'
 import App from '../../src/components/app-wrapper'
 import * as utils from '../utils'
-import TestUtils from 'react-addons-test-utils'
 
 describe('App', () => {
   const component = utils.shallowlyRenderedOutput(<App />)
--- test/components/counter.spec.js
+++ test/components/counter.spec.js
@@ -1,55 +1,58 @@
-import React from 'react';
-import Counter from '../../src/components/Counter';
-import jsdom from 'mocha-jsdom';
-import TestUtils from 'react-addons-test-utils';
+// Counter component spec
+// ======================
+
+import React from 'react'
+import Counter from '../../src/components/Counter'
+import jsdom from 'mocha-jsdom'
+import TestUtils from 'react-addons-test-utils'
 
 describe('Components', () => {
-  jsdom();
+  jsdom()
+
   describe('Counter', () => {
     // Mock minimal Home interface
-    const Home = React.createClass({
-
-      getInitialState() {
-        return {
-          counter: 0
-        };
-      },
+    class Home extends React.Component {
+      constructor(options = {}) {
+        super(options)
+        this.state = { counter: 0 }
+      }
 
       increment() {
-        this.setState({
-          counter: this.state.counter += 1
-        });
-      },
+        this.setState({ counter: this.state.counter += 1 })
+      }
 
       render() {
-        return (
-          <div>
-            <Counter
-              count={this.state.counter}
-              onIncrement={this.increment}
-            />
-          </div>
-        );
+        return <div>
+          <Counter count={this.state.counter} onIncrement={this.increment}
+          />
+        </div>
       }
-    });
+    }
 
     it('should receive and increment counter', () => {
-      const tree = TestUtils.renderIntoDocument(<Home />);
-      const counter = TestUtils.findRenderedComponentWithType(tree, Counter);
-      TestUtils.Simulate.click(TestUtils.findRenderedDOMComponentWithTag(counter, 'button'));
-      expect(counter.props.count).to.be.equal(1);
-      expect(TestUtils.findRenderedDOMComponentWithTag(counter, 'h1').textContent)
-        .to.contain('1');
-    });
+      const tree = TestUtils.renderIntoDocument(<Home />)
+      const counter = TestUtils.findRenderedComponentWithType(tree, Counter)
+      TestUtils.Simulate.click(
+        TestUtils.findRenderedDOMComponentWithTag(counter, 'button')
+      )
+      expect(counter.props.count).to.be.equal(1)
+      expect(
+        TestUtils.findRenderedDOMComponentWithTag(counter, 'h1').textContent
+      ).to.contain('1')
+    })
 
     describe('increment', () => {
       it('should get called when a click on button happens', () => {
-        const spy = sinon.spy();
-        const counter = TestUtils.renderIntoDocument(<Counter onIncrement={spy} count={0} />);
-        const button = TestUtils.findRenderedDOMComponentWithTag(counter, 'button');
-        TestUtils.Simulate.click(button);
-        expect(spy).to.have.been.calledOnce;
-      });
-    });
-  });
-});
+        const spy = sinon.spy()
+        const counter = TestUtils.renderIntoDocument(
+          <Counter onIncrement={spy} count={0} />
+        )
+        const button = TestUtils.findRenderedDOMComponentWithTag(
+          counter, 'button'
+        )
+        TestUtils.Simulate.click(button)
+        expect(spy).to.have.been.calledOnce
+      })
+    })
+  })
+})
