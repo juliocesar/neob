8e47a476dccda739fbdd2ab7a484eadc08d87cff
Julio Cesar Ody <julio@awesomebydesign.com>
2015-10-20 15:20:29 +1100

    Production server lint fixes

--- bin/server.production.js
+++ bin/server.production.js
@@ -1,16 +1,18 @@
-var path = require('path');
-var express = require('express');
-var app = express();
+// Production server
+// =================
 
-app.use(express.static(__dirname + '/dist'));
+import path from 'path'
+import express from 'express'
+
+let app = express()
+
+app.use(express.static(__dirname + '/dist'))
 
 app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
-});
+  res.sendFile(path.join(__dirname, 'dist/index.html'))
+})
 
 app.listen(4567, 'localhost', function(err) {
-  if (err) {
-    console.log(err);
-  }
-  console.info("==> 🌎 Listening on port %s. Open up http://localhost:%s/ in your browser.", 4567, 4567);
-});
+  if (err) console.log(err)
+  console.info(` ==> Listening on port 4567`)
+})
