12bec4d0a5f03ca190f6b3d3c6dd05cb73f861fa
Julio Cesar Ody <julio@awesomebydesign.com>
2016-06-17 11:17:42 +1000

    Just move the counter test for now

---
+++ src/components/counter/test/counter.test.js
@@ -0,0 +1,11 @@
+// Counter component test
+// ======================
+
+import React from 'react'
+import Counter from '../'
+import test from 'ava'
+import { shallow } from 'enzyme'
+
+test('Counter', t => {
+  t.pass()
+})
