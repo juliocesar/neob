92f5057dc732145b221c801b086b32a7e20ee348
Julio Cesar Ody <julio@awesomebydesign.com>
2016-06-17 14:46:24 +1000

    Re-add react-router

--- src/boot.js
+++ src/boot.js
@@ -6,11 +6,8 @@ import './stylesheets/top-wrappers.css'
 import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
-import Router from 'react-router'
-import createHashHistory from 'history/lib/createHashHistory'
+import { Router, hashHistory as history } from 'react-router'
 import routes from './routes'
-
-const history = createHashHistory()
 
 render(
   <Router children={routes} history={history} />,
